5738bd1f3e4dc0f65d7846728e500c1e7f173730
Roy Sand <roy@sanddata.no>
2023-11-20 22:13:07 +0100

    tested repository with .net 8

--- apps/ConsoleAppTest/App.cs
+++ apps/ConsoleAppTest/App.cs
@@ -1,4 +1,5 @@
 using Application.Common.Interface;
+using Domain.Entities;
 using Infrastructure.Services;
 
 namespace ConsoleAppTest;
@@ -6,13 +7,30 @@ namespace ConsoleAppTest;
 public class App
 {
     private readonly IDateTime _dateTimeService;
+    private readonly IDetailRepository<Detail> _detailRepository;
 
-    public App(IDateTime dateTimeService)
+    public App(IDateTime dateTimeService, IDetailRepository<Detail> detailRepository)
     {
         _dateTimeService = dateTimeService;
+        _detailRepository = detailRepository;
     }
-    public void Run(string[] args)
+    public async Task Run(string[] args)
     {
         Console.WriteLine($"Hello world from App: {_dateTimeService.NowLocalTime}");
+        try
+        {
+            var result = await _detailRepository.Find(w => w.Id == new Guid("6d0d3a94-059e-481e-bf91-f3d65144af59"),
+                new CancellationToken());
+
+            Console.WriteLine($"Number of rows {result.Count()}");
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+
     }
 }
