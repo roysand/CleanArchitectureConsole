a2ba88b8d819704ce8d222d4f636c8e8a719e55d
Roy Sand <roy@sanddata.no>
2023-11-20 22:44:15 +0100

    Feature/upgrade to net8 (#2)
    * Upgrade to .net 8
    
    * upgraded to .net 8
    
    * tested repository with .net 8
    
    * tested repository with .net 8

--- apps/ConsoleAppTest/Program.cs
+++ apps/ConsoleAppTest/Program.cs
@@ -2,30 +2,43 @@
 
 using ConsoleAppTest;
 using Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .AddJsonFile("local.settings.json")
+    .Build();
+
 // create hosting object and DI layer
-using IHost host = CreateHostBuilder(args).Build();
+using IHost host = CreateHostBuilder(args, configuration).Build();
+
 // create a service scope
 using var scope = host.Services.CreateScope();
 
 var services = scope.ServiceProvider;
+
 try
 {
-    services.GetRequiredService<App>().Run(args);
+    await services.GetRequiredService<App>().Run(args);
 }
 catch (Exception e)
 {
     Console.WriteLine(e.Message);
 }
 
-IHostBuilder CreateHostBuilder(string[] strings)
+IHostBuilder CreateHostBuilder(string[] strings, IConfiguration configuration)
 {
     return Host.CreateDefaultBuilder()
         .ConfigureServices((_, services) =>
         {
             services.AddInfrastructure();
             services.AddSingleton<App>();
+        })
+        .ConfigureHostConfiguration(hostConfig =>
+        {
+            hostConfig.AddConfiguration(configuration);
         });
 }
